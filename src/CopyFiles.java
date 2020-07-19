084638aa84de6f89e436416b8a4f7bcb7269cd80
Qutada Samaha <qsamaha98@gmail.com>
2020-07-19 14:36:37 -0400

    Copied fileBranches

---
+++ src/CopyFiles.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class CopyFiles extends SimpleFileVisitor<Path> {
+
+    private Path sourceRoot;
+    private Path targetPath;
+
+    public CopyFiles(Path sourceRoot, Path targetPath) {
+        this.sourceRoot = sourceRoot;
+        this.targetPath = targetPath;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        System.out.println("Error accessing file: " + file.toAbsolutePath() + " " + exc.getMessage());
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path relativizedPath = sourceRoot.relativize(dir);
+        System.out.println("RelativizedPath = " + relativizedPath);
+        Path copyDir = targetPath.resolve(relativizedPath);
+        System.out.println("Resolved path for copy = " + copyDir);
+
+        try {
+            Files.copy(dir, copyDir);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return FileVisitResult.SKIP_SUBTREE;
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path relativizedPath = sourceRoot.relativize(file);
+        System.out.println("RelativizedPath = " + relativizedPath);
+        Path copyDir = targetPath.resolve(relativizedPath);
+        System.out.println("Resolved path for copy = " + copyDir);
+
+        try {
+            Files.copy(file, copyDir);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+}
